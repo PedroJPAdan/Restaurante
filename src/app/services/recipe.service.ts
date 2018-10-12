a61ab6493ce14ef95ec2009118c2d3d0fac64217
PedroJPAdan <pedro.abmodel@gmail.com>
2018-10-12 16:36:09 -0500

    nuevo comentario

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -6,17 +6,17 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-        new Recipe('A test recipe 1', 'This is a simply test ', 'https://cocina-casera.com/mx/wp-content/uploads/2018/01/tacoas-de-carne-enchilada.jpg', 
+        new Recipe('A test recipe 1', 'This is a simply test ', 'http://sevilla.abc.es/contenidopromocionado/wp-content/uploads/sites/2/2017/09/1996x1206-hamburguesas.jpg', 
     [
         new Ingredient('bread', 2),
         new Ingredient('tomatoes', 3)
     ]),
-        new Recipe('A test recipe 2', 'This is a simply test', 'https://cocina-casera.com/mx/wp-content/uploads/2018/01/tacoas-de-carne-enchilada.jpg',
+        new Recipe('A test recipe 2', 'This is a simply test', 'http://sevilla.abc.es/contenidopromocionado/wp-content/uploads/sites/2/2017/09/1996x1206-hamburguesas.jpg',
         [
             new Ingredient('Beef', 3),
             new Ingredient('garlic', 1)
         ]),
-        new Recipe('A test recipe 3', 'This is a simply test', 'https://cocina-casera.com/mx/wp-content/uploads/2018/01/tacoas-de-carne-enchilada.jpg',
+        new Recipe('A test recipe 3', 'This is a simply test', 'http://sevilla.abc.es/contenidopromocionado/wp-content/uploads/sites/2/2017/09/1996x1206-hamburguesas.jpg',
         [
             new Ingredient('Sauce', 2),
         ])
